25bcfcfb48e00d8ee739be75147db3120a315d9c
张迪 <codeway3@126.com>
2018-01-22 15:02:05 +0800

    add form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import os
-from flask import send_from_directory, render_template
+from flask import send_from_directory, render_template, redirect, flash
 from app import app
+from .form import LoginForm
 
 
 @app.route('/favicon.ico')
@@ -13,3 +14,15 @@ def favicon():
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign in',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
