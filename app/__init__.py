410b8f93c1267376852de973948475eb5d8944ee
张迪 <codeway3@126.com>
2018-01-24 16:54:46 +0800

    add database and config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+track_modifications = app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
