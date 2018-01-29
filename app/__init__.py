738fd78fab7bac8bd969781d4a701d5fafe1bede
张迪 <codeway3@126.com>
2018-01-29 18:10:24 +0800

    add login & logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
+from flask_login import LoginManager
+from flask_openid import OpenID
 from flask_sqlalchemy import SQLAlchemy
+from config import basedir
 
 
 app = Flask(__name__)
@@ -7,4 +11,10 @@ app.config.from_object('config')
 track_modifications = app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
 db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 from app import views, models
+
